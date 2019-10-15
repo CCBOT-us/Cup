710e3416d56b0f92032b9d9bccc5cda83b358f04
nt_zi_mu <1540003703@qq.com>
2019-10-15 21:40:14 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ var __apiKeys = [
 	'm782067687-1a84ed3c73099711e0a5855e', //Site 2
 	'm783580933-90763ad6b0313f896d40675b', //DEV
 	'm782067763-d7d74e1e374366dcf10cc952', //Api
-	'm783580933-90763ad6b0313f896d40675b', //ApiE
+	'm783628197-81d34c00261a0a2d3e9b96d6', //ApiE
 ];
 
 // refresh interval (in seconds)
